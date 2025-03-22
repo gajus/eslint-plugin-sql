9b2f5e1654ac95dd6b59825826c1ea36a658e2f7
Gajus <gajus@gajus.com>
2025-03-21 20:42:27 -0600

    fix: adopt typescript-eslint

--- src/utilities/dropBaseIndent.ts
+++ src/utilities/dropBaseIndent.ts
@@ -3,10 +3,14 @@ export const dropBaseIndent = (raw: string) => {
   const trimmedString = raw.replace(/ *$/u, '');
 
   const matches = trimmedString.match(/^[\t ]*(?=\S)/gmu);
-  if (!matches) return trimmedString;
+  if (!matches) {
+    return trimmedString;
+  }
 
   const indent = Math.min(...matches.map((match) => match.length));
-  if (!indent) return trimmedString;
+  if (!indent) {
+    return trimmedString;
+  }
 
   return trimmedString.replaceAll(new RegExp(`^[ \\t]{${indent}}`, 'gmu'), '');
 };
